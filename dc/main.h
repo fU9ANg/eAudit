ba3167ff9aa9be8f378b745a50fa81232d84e0e6
fU9ANg <bb.newlife@gmail.com>
2013-04-27 00:56:46 +0800

    added macro

---
+++ dc/main.h
@@ -0,0 +1,20 @@
+
+/*
+ * file: main.h
+ * Written 2009-2013 by fU9ANg
+ * bb.newlife@gmail.com
+ *
+ */
+
+#ifndef _DC_MAIN_H
+#define _DC_MAIN_H
+
+#define RECV_BYTES_LEN 256
+#define REGISTER_INFO   0x01
+#define UNREGISTER_INFO   0x02
+#define REGISTER_RESPONSE_CODE 0x81
+#define UNREGISTER_RESPONSE_CODE 0x82
+
+#define DC_USR_NUM 4000
+
+#endif // _DC_MAIN_H
