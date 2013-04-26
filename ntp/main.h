7617637f20213fdf9af1ec872e1c473c7b2d5da5
fU9ANg <bb.newlife@gmail.com>
2013-04-27 00:24:45 +0800

    added main function

---
+++ ntp/main.h
@@ -0,0 +1,15 @@
+
+/*
+ * file: main.h
+ * written 2009, 2010, 2011, 2012, 2013 by fU9ANg
+ * bb.newlife@gmail.com
+ */
+
+#ifndef _NTP_MAIN_H
+#define _NTP_MAIN_H
+
+extern NTP_TIMERSYN_SERVER_ID    g_ntp_timersyn_server_id;
+extern HANDMODIFYTIME_SERVER_ID  g_handmodify_server_id ;
+extern SECOND_TIMERSYN_SERVER_ID g_second_timersyn_server_id;
+
+#endif
